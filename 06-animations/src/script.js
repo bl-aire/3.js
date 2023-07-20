6511657f0f2c9cf1c58ead86032fc566e8192bac
Ogoh Blessing <ogoh_blessing@yahoo.com>
2023-07-20 17:42:02 +0100

    Using requestAnimationFrame

--- 06-animations/src/script.js
+++ 06-animations/src/script.js
@@ -23,9 +23,28 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
 
+
 // Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+//renderer.render(scene, camera)
+
+/**
+ * Animation
+ */
+
+const tick = () => {
+    // Update objects
+    mesh.rotation.y += 0.01
+
+    // Render
+    renderer.render(scene, camera)
+    
+    // Call tick again on the next frame
+    
+    window.requestAnimationFrame(tick)
+}
+
+tick()
