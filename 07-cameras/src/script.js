1d853ed9a259816e6190795fa6ceafd04684a74d
Ogoh Blessing <ogoh_blessing@yahoo.com>
2023-07-31 16:03:38 +0100

    PerspectiveCamera and OrthographicCamera

--- 07-cameras/src/script.js
+++ 07-cameras/src/script.js
@@ -17,13 +17,19 @@ const scene = new THREE.Scene()
 
 // Object
 const mesh = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+    new THREE.BoxGeometry(1, 1, 1, 5, 5, 5), //width, height, depth, widthSegment, heightSegment, depthSegment. Default _ 1
     new THREE.MeshBasicMaterial({ color: 0xff0000 })
 )
 scene.add(mesh)
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+//const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 1, 100) //FOV, aspect ratio, near, far. PERSPECTIVE CAMERA
+
+//const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 100) // LRTB, near, far. ORTHOGRAPHIC CAMERA (due to LRTB, the square rendered stretches to fit the canvas which is rectangular so it is distorted. We need to use aspect ratio)
+
+const aspectRatio = sizes.width / sizes.height
+const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, 0.1, 100)
+
 camera.position.x = 2
 camera.position.y = 2
 camera.position.z = 2
