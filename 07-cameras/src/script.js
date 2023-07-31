060702443df9d4ba6dd1f42fdb06885c35596b08
Ogoh Blessing <ogoh_blessing@yahoo.com>
2023-07-31 16:29:24 +0100

    Custom control - control camera using a mouse

--- 07-cameras/src/script.js
+++ 07-cameras/src/script.js
@@ -23,16 +23,16 @@ const mesh = new THREE.Mesh(
 scene.add(mesh)
 
 // Camera
-//const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 1, 100) //FOV, aspect ratio, near, far. PERSPECTIVE CAMERA
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 1, 100) //FOV, aspect ratio, near, far. PERSPECTIVE CAMERA
 
 //const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 100) // LRTB, near, far. ORTHOGRAPHIC CAMERA (due to LRTB, the square rendered stretches to fit the canvas which is rectangular so it is distorted. We need to use aspect ratio)
 
-const aspectRatio = sizes.width / sizes.height
-const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, 0.1, 100)
+//const aspectRatio = sizes.width / sizes.height
+//const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, 0.1, 100)
 
-camera.position.x = 2
-camera.position.y = 2
-camera.position.z = 2
+//camera.position.x = 2
+//camera.position.y = 2
+camera.position.z = 3
 camera.lookAt(mesh.position)
 scene.add(camera)
 
@@ -42,6 +42,19 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 
+// Cursor
+const cursor = {
+    x: 0,
+    y: 0
+}
+
+window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX /sizes.width - 0.5
+    cursor.x = event.clientY /sizes.height - 0.5
+
+    console.log(cursor.x, cursor.y) //mouse position is stored in cursor( 0 = center, -0.5 = far left, 0.5 = far right)
+})
+
 // Animate
 const clock = new THREE.Clock()
 
@@ -50,7 +63,11 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    mesh.rotation.y = elapsedTime;
+    //mesh.rotation.y = elapsedTime;
+
+    //Update camera
+    camera.position.x = cursor.x
+    camera.position.y = cursor.y
 
     // Render
     renderer.render(scene, camera)
